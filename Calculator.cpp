b069d24958ba8c87805a8250255223a79c3404ac
Swagat2 <shreemanswagat2002@gmail.com>
2026-02-28 12:24:35 +0530

    added new code

--- Calculator.cpp
+++ Calculator.cpp
@@ -2,7 +2,32 @@
 using namespace std;
 int main(){
     char oper;
-    cout<<"Enter an operator (+, -, * , / ) : ";
+    cout<<"Enter an operator (+, -, * , / , % ) : ";
     cin >> oper;
+    char num1, num2;
+    cout<<"Enter two number : "<<endl;
+    cin >> num1 >> num2;
 
+    switch (oper)
+    {
+    case '+':
+        cout << num1 << " + " << num2 <<" = " <<num1+num2;
+        break;
+    case '-':
+        cout << num1 << " - " << num2 <<" = " <<num1-num2;
+        break;
+    case '*':
+        cout << num1 << " * " << num2 <<" = " <<num1*num2;
+        break;
+    case '/':
+        cout << num1 << " / " << num2 <<" = " <<num1/num2;
+        break;
+    case '%':
+        cout << num1 << " % " << num2 <<" = " <<num1%num2;
+        break;
+    default:
+        cout << "Error! The operator is not correct";
+        break;
+    }
+    return 0;
 }
